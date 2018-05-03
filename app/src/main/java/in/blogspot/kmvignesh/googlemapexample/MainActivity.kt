92ec267023bf7b6be43efd116913a107286388e9
VickY <kmvignesh94@gmail.com>
2018-05-03 08:35:10 +0530

    Initial commit

---
+++ app/src/main/java/in/blogspot/kmvignesh/googlemapexample/MainActivity.kt
@@ -0,0 +1,39 @@
+package `in`.blogspot.kmvignesh.googlemapexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            googleMap.isMyLocationEnabled = true
+            val location1 = LatLng(13.03,77.60)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+
+            val location2 = LatLng(9.89,78.11)
+            googleMap.addMarker(MarkerOptions().position(location2).title("Madurai"))
+
+
+            val location3 = LatLng(13.00,77.00)
+            googleMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+
+        })
+    }
+}
